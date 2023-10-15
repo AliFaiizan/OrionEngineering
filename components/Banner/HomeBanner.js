2fd4bf742f001ea974513cdc3e3cbbd1cf43bad1
alifaiizan <alifaizanahmad@gmail.com>
2023-10-16 00:22:59 +0200

    initial commits

---
+++ components/Banner/HomeBanner.js
@@ -0,0 +1,67 @@
+import { BadgeGroup, BadgeIcon, BadgeMessage } from "@components/Badge";
+import { Button, ButtonGroup } from "@components/Button";
+import { Content } from "@components/Content";
+import { MotionBTTContainer, MotionInfiniteImage } from "@components/Motion";
+import { SectionContainer } from "@components/Section";
+import { PageTitle } from "@components/Title";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+
+export const HomeBanner = () => {
+    return (
+        <SectionContainer className="page-banner--container py-16">
+            <SectionContainer className="page-banner--inner-container wrap wrap-px z-10">
+                {/* Appear First */}
+                <MotionBTTContainer transition={{ delay: 0.2, duration: 0.5 }}>
+                    <BadgeGroup alignment="center">
+                        <BadgeMessage>Discover Orion Engineering!</BadgeMessage>
+                        <BadgeIcon icon="game-icons:meal" />
+                    </BadgeGroup>
+                </MotionBTTContainer>
+                {/* Appear Second */}
+                <MotionBTTContainer transition={{ delay: 0.4, duration: 0.5 }}>
+                    <PageTitle className="text-center mx-auto" type="heavy">
+                        Welcome to Orion Engineering, where engineering
+                        excellence meets aesthetic brilliance.
+                    </PageTitle>
+                </MotionBTTContainer>
+                {/* Appear Third */}
+                <MotionBTTContainer transition={{ delay: 0.6, duration: 0.5 }}>
+                    <Content className="text-center" alignment="center">
+                        <p>
+                            {" "}
+                            From cutting-edge ceiling fans to a wide array of
+                            engineering solutions, we redefine comfort, style,
+                            and functionality{" "}
+                        </p>
+                    </Content>
+                    <div className="mt-6 mb-16 text-center">
+                        <ButtonGroup alignment="center">
+                            <a
+                                role="button"
+                                href="#features"
+                                className="btn btn--secondary lemonsqueezy-button ml-4"
+                            >
+                                Products
+                                <Icon icon="material-symbols:arrow-forward-rounded" />
+                            </a>
+                        </ButtonGroup>
+                    </div>
+                </MotionBTTContainer>
+                {/* Appear Fourth */}
+                <MotionBTTContainer transition={{ delay: 0.8, duration: 0.5 }}>
+                    <div className="page-banner--image">
+                        <Image
+                            src="/fan2.png"
+                            width={680}
+                            height={680}
+                            alt="Page Banner"
+                            objectFit="cover"
+                            className="mx-auto"
+                        />
+                    </div>
+                </MotionBTTContainer>
+            </SectionContainer>
+        </SectionContainer>
+    );
+};
